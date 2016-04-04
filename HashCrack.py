c74a0b94f71d76caed047419260501639dc3e55c
wh0am3y3 <43875109+wh0am3y3@users.noreply.github.com>
2016-04-04 14:53:04 +0300

    added more functionality to script to check all word uppercase or uppercase first letter

--- HashCrack.py
+++ HashCrack.py
@@ -2,25 +2,47 @@
 import hashlib
 
 
-def dict(password_hash):
-    dictionary = ['letmain', 'password', '12345', 'football']
+def dictionary_attack(password_hash):
+    with open(raw_input('Enter File name : '), 'r') as fp:
+        for line in fp:
+            line = line.replace("\n", "")
+            dictionary = line
+            fp.close()
+    # dictionary = ['letmein', 'password', '12345', 'football']
     password_found = False
     for dictionary_value in dictionary:
-        hashed_value = (hashlib.md5(dictionary_value)).hexdigest
+        hashed_value = (hashlib.md5(dictionary_value)).hexdigest()
+        hashed_value_upper = (hashlib.md5(dictionary_value.upper())).hexdigest()
+        hashed_value_UpperLetter = (hashlib.md5(dictionary_value.title())).hexdigest()
         if hashed_value == password_hash:
             password_found = True
             recoverd_password = dictionary_value
-    if password_found is True:
-        print('Found match for hash \n', password_hash)
-        print('Password recoverd: ', recoverd_password)
+        elif hashed_value_upper == password_hash:
+            password_found = True
+            recoverd_password = dictionary_value.upper()
+        elif hashed_value_UpperLetter == password_hash:
+            password_found = True
+            recoverd_password = dictionary_value.title()
+            break
+    if password_found == True:
+        print 'Found match for hash \n', password_hash
+        print 'Password recoverd: ', recoverd_password
     else:
-        print('Password not found')
+        print 'Password not found'
 
 
 def main():
-    password_hash = input('Enter md5 hash: ')
-    dict(password_hash)
+    # password_hash = raw_input('Enter md5 hash: ')
+    try:
+        with open(raw_input('Enter hash file: '), 'r') as pf:
+            for passwd in pf:
+                passwd = passwd.replace("\n", "")
+                password_hash = passwd
+                pf.close()
 
+        dictionary_attack(password_hash)
+    except KeyboardInterrupt:
+        print '<-- Ctrl-C pressed exited'
 
 if __name__ == '__main__':
     main()
