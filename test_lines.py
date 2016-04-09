a70d7046116275f32939c457246b4c8a7e6579cf
wh0am3y3 <43875109+wh0am3y3@users.noreply.github.com>
2016-04-09 08:29:10 +0300

    added partial code fb names with birthdays

--- test_lines.py
+++ test_lines.py
@@ -1,41 +1,62 @@
 #!/usr/bin/env python
 from __future__ import print_function
-from string import swapcase, maketrans
+from string import swapcase, maketrans, rstrip
 from symbols import *
 from numbers import *
 
 # dictionary = open(raw_input('Enter dictionary file: '), 'r').readlines()
-dictionary = open('dict3.txt', 'r').readlines()
-# def create():
+
+folder = ['fb-fn.txt', 'birthdays.txt']
+with open('fb-fn-birthdays.txt', 'w') as outfile:
+    for file in folder:
+        with open(file[0]) as newfile, open(file[1]) as newfile1:
+            lines = zip(newfile, newfile1)
+            for line in lines:
+                outfile.write(line[0]).strip()+line[1])
+                with open('fb-fn-birthdays.txt', 'r').readlines() as fb_fn_dictionary:
+                    for fb_fn_birthdays_value in fb_fn_dictionary:
+                        fb-fn-b_value = fb_fn_birthdays_value.strip('\n')
+                        print(fb-fn-b_value)
 
 
+
+
+dictionary = open('dict.txt', 'r').readlines()
 for dictionary_value in dictionary:
     dictionary_value = dictionary_value.strip('\n')
     value = dictionary_value
     value_upper = dictionary_value.upper()
     value_upperletter = dictionary_value.title()
     # f = open('hash.txt', 'a')
-    # print(value)
-    # print(value_upper)
-    # print(value_upperletter)
+    print(value)
+    print(value_upper)
+    print(value_upperletter)
     if value_upperletter:
         value_swap = swapcase(dictionary_value.title())
-        # print(value_swap)
+        print(value_swap)
     if value:
         intab = "alieos"
-        outtab = "41!30$"
+        outtab = "@1!30$"
         value_leet = maketrans(intab, outtab)
         val = value
         value_leet_translate = val.translate(value_leet)
-        # print(value_leet_translate)
+        print(value_leet_translate)
     if value:
         for i in numbers_list:
-            append_numbers = dictionary_value + str(i)
-            # print(append_numbers)
+            append_numbers = value + str(i)
+            append_numbers = value_upper + str(i)
+            append_numbers = value_upperletter + str(i)
+            append_numbers = value_swap + str(i)
+            append_numbers = value_leet_translate + str(i)
+            print(append_numbers)
     if value:
         for i in symbols_list:
-            append_symbols = dictionary_value + str(i)
-            # print(append_symbols)
+            append_symbols = value + str(i)
+            append_symbols = value_upper + str(i)
+            append_symbols = value_upperletter + str(i)
+            append_symbols = value_swap + str(i)
+            append_symbols = value_leet_translate + str(i)
+            print(append_symbols)
 
         # return create()
         # f.close()
